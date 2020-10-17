f18b7b2b7905e2c20efccaecc477f87aa6f3273c
Riccardo Cogoni <cogonir@live.com>
2020-10-18 00:34:51 +0100

    app init

---
+++ src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to my App!! 👀</h2>
+      </div>
+    );
+  }
+}
